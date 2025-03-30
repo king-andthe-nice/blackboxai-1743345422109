66c49b8c86ecd27a2f5467b1cb4bb9c9e9a02120
king-andthe-nice <3356369582@qq.com>
2025-03-30 22:37:24 +0800

    Add app.vue 1743345444164

---
+++ app.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  onLaunch() {
+    console.log('App Launch')
+  },
+  onShow() {
+    console.log('App Show')
+  },
+  onHide() {
+    console.log('App Hide')
+  }
+}
+</script>
+
+<style>
+/* Import Tailwind CSS */
+@import url('https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css');
+
+/* Base styles */
+#app {
+  font-family: 'PingFang SC', 'Helvetica Neue', Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #333;
+  max-width: 100vw;
+  min-height: 100vh;
+  background-color: #f5f5f5;
+}
+</style>
